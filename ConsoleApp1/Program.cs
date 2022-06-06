c958144d21d0cbeff8f98b10bab8bf26b03b04cd
HECTOR MARCELO HIDALGO QUENA <hector.hidalgo@ucb.edu.bo>
2022-06-06 13:50:34 -0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Ucb.Encryption.Interfaces;
+using Ucb.Encryption.Repositories;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             IEncryptionHelper helper = new EncryptionHelper();
+            Console.WriteLine(helper.DecryptParameterId("KFHHPZw8mfB%252bBhaOFtjNiw==")); //20007911, 20000043
+            Console.ReadKey();
+        }
+
+        // Enum   
+        public enum Weekday
+        {
+            Monday = 0, Tuesday = 1, Wednesday = 2, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7
+        }
+    }
+}
